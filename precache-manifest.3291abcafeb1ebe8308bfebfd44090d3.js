6cd5927ff1ae1b8cd32539c9aaa4f3f57f7a9d99
AliD201 <AliD201@users.noreply.github.com>
2020-07-25 22:04:37 +0300

    Updates

---
+++ precache-manifest.3291abcafeb1ebe8308bfebfd44090d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151d1f29e0ea50fe7784cc92ca8ae3f8",
+    "url": "/SmartBrain-Redux/index.html"
+  },
+  {
+    "revision": "c3adeab025c417b9217b",
+    "url": "/SmartBrain-Redux/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e9c5ac0277e0885be398",
+    "url": "/SmartBrain-Redux/static/css/main.553e14bb.chunk.css"
+  },
+  {
+    "revision": "c3adeab025c417b9217b",
+    "url": "/SmartBrain-Redux/static/js/2.4d879ebe.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/SmartBrain-Redux/static/js/2.4d879ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c5ac0277e0885be398",
+    "url": "/SmartBrain-Redux/static/js/main.d95c66ba.chunk.js"
+  },
+  {
+    "revision": "ee77f6a1d3bc40c013d7",
+    "url": "/SmartBrain-Redux/static/js/runtime-main.a3a15bb7.js"
+  },
+  {
+    "revision": "8348a5c0a40c6aeec237cf417b1ad77e",
+    "url": "/SmartBrain-Redux/static/media/brain.8348a5c0.png"
+  }
+]);
