5bf6a7593c7f9eb4436a85884152d31f3f595b55
AliD201 <AliD201@users.noreply.github.com>
2020-07-25 22:17:52 +0300

    fixing default input

--- src/Compnents/ImageLinkForm/ImageLinkForm.js
+++ src/Compnents/ImageLinkForm/ImageLinkForm.js
@@ -11,7 +11,7 @@ const ImageLinkForm = (props) => {
       </p>
       <div  style = {{ display: 'flex', justifyContent:'center'}}>
         <div className=" shadow-2 pa4 w-60 form" >
-          <input className="f4  w-70 pa2" type="text" onChange={onInputChange} defaultValue ="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTEpOB50Gf-839DMNoHRlYYBAWP0TN73RHsGg&usqp=CAU"></input>
+          <input className="f4  w-70 pa2" type="text" onChange={onInputChange} defaultValue ="https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/03/GettyImages-1092658864_hero-1024x575.jpg?w=1155&h=1528"></input>
           <button className="f3 grow w-30 link white dib bg-light-purple ph3 pv2" onClick={onSubmit}>Detect</button>
         </div>
       </div>
--- src/Redux/reducers.js
+++ src/Redux/reducers.js
@@ -9,7 +9,7 @@ import {
 
 const initialState = {
   input:'',
-  imageUrl:'',
+  imageUrl:'https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/03/GettyImages-1092658864_hero-1024x575.jpg?w=1155&h=1528',
   box :{},
 
 }
